90ecf71cd531659d5db9bafd7e2e057e74c73d08
github username <github email address>
2020-02-13 20:46:39 -0800

    adding sign-in form

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,25 +1,11 @@
 class StudentsController < ApplicationController
 
-    def new
-        @student = Student.new
-    end
-
-    # def create
-    #     @student = Student.new(student_params)
-    #     if @student.save
-    #         session[:student_id] = @student.id
-    #         redirect_to @student
-    #     else
-    #         render :new
-    #     end
-    # end
-
-
     def show
         @student = Student.find(params[:id])
     end
 
-    def student_params
-        params.require(:student).permit(:username, :email, :password)
+    def new
+        @student = Student.new
     end
+
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,17 +1,20 @@
 class Student < ApplicationRecord
 
-    before_save {self.email = email.downcase}
+    # before_save {self.email = email.downcase}
 
-    validates :username, presence: true, length: { maximum: 20 }, uniqueness: true
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-    validates :email, presence: true, length: { maximum: 50 },
-                    format: { with: VALID_EMAIL_REGEX }, 
-                    uniqueness: true
+    # validates :username, presence: true, length: { maximum: 20 }
+    # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    # validates :email, presence: true, length: { maximum: 50 },
+    #                 format: { with: VALID_EMAIL_REGEX }, 
+    #                 uniqueness: true
+
+    validates :username, presence: true
+    validates :email, presence: true                
 
     has_many :appointments
     has_many :tutors, through: :appointments
 
     has_secure_password
-    validates :password, length: { maximum: 7 }
+    # validates :password, length: { maximum: 7 }
 
 end
