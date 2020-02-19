ed4ea4a2445d9f64682b668af6fc651f63cf7fac
github username <github email address>
2020-02-18 19:58:16 -0800

    creating appoinments

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,10 +1,16 @@
 class AppointmentsController < ApplicationController
 
-    # def new
-    #     @appointment = Appointment.new
-    # end
+    def new
+        @appointment = Appointment.new
+    end
 
     def show
         @appointment = Appointment.find(params[:id])
+    end
+
+    private
+
+    def appointment_params
+        params.require(:appointment).permit(appointment_time)
     end
 end
